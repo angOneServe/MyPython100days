221eb719caf475722ce16e17ac9585536ae915eb
angOneServe <1520124924@qq.com>
2019-11-16 19:52:15 +0800

    闭包的理解，笔记中加入了一个博客网址，里面解释的比较好，b站av18586448对闭包域装饰器这一块也讲解得比较好，自身的练习还待完善

--- day16-20/1_函数的使用方式/2_Python搜索变量的顺序.py
+++ day16-20/1_函数的使用方式/2_Python搜索变量的顺序.py
@@ -3,12 +3,35 @@
 #再看全局作用域
 #最后看内置作用域
 
-#闭包内饰域嵌套定义函数，内部函数可以访问外层的值
+#嵌套定义函数，返回内存函数后，外层函数被回收，这个内存函数保存则外层函数里的变量
 
-def WaiFunc():
-    a=1
-    a=lambda a:a*2
-    return a
+def func1():
+    temp=[1,2,3]
+    print("outer print(id(temp))",id(temp))
+    def func2():
+        temp[0]+=1
+        print("iner print(id(temp))",id(temp))
+        print(temp[0])
+    return func2
 
-print(WaiFunc())
+var=func1()
+var()
+var()
+var()
+var()
+print("\n\n\n")
+def func1():
+    temp=1
+    print("outer print(id(temp))",id(temp))
+    def func2():
+        nonlocal temp
+        temp+=1
+        print("iner print(id(temp))",id(temp))
+        print(temp)
+    return func2
 
+var=func1()
+var()
+var()
+var()
+var()
