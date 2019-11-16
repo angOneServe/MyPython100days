70b4ead548f3ba3894c378546d6553eb91ce0113
angOneServe <1520124924@qq.com>
2019-11-16 20:01:24 +0800

    闭包代码练习

--- day16-20/1_函数的使用方式/2_Python搜索变量的顺序.py
+++ day16-20/1_函数的使用方式/2_Python搜索变量的顺序.py
@@ -3,35 +3,3 @@
 #再看全局作用域
 #最后看内置作用域
 
-#嵌套定义函数，返回内存函数后，外层函数被回收，这个内存函数保存则外层函数里的变量
-
-def func1():
-    temp=[1,2,3]
-    print("outer print(id(temp))",id(temp))
-    def func2():
-        temp[0]+=1
-        print("iner print(id(temp))",id(temp))
-        print(temp[0])
-    return func2
-
-var=func1()
-var()
-var()
-var()
-var()
-print("\n\n\n")
-def func1():
-    temp=1
-    print("outer print(id(temp))",id(temp))
-    def func2():
-        nonlocal temp
-        temp+=1
-        print("iner print(id(temp))",id(temp))
-        print(temp)
-    return func2
-
-var=func1()
-var()
-var()
-var()
-var()
---
+++ day16-20/1_函数的使用方式/3_闭包.py
@@ -0,0 +1,35 @@
+#嵌套定义函数，返回内存函数后，外层函数被回收，这个内存函数保存则外层函数里的变量
+
+#如果这个变量是可变类型,地址一直不变
+def func1():
+    d={"key":1}
+    print("outer d地址为)",id(d))
+    def func2():
+        d["key"]+=1
+        print("iner d地址为)",id(d))
+        print(d["key"])
+    return func2
+
+var=func1()
+var()
+var()
+var()
+var()
+print("\n\n\n")
+
+#如果这个变量是不可变类型,改变变量值地址会变【这是不可变类型变量的特性】，且在内部使用需要nonlocal
+def func1():
+    temp=1
+    print("outer id(temp)",id(temp))
+    def func2():
+        nonlocal temp
+        temp+=1
+        print("iner id(temp)",id(temp))
+        print(temp)
+    return func2
+
+var=func1()
+var()
+var()
+var()
+var()
